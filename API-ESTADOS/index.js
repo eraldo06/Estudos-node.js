4cc3b868a792babcab05d21700c63439efcdd568
eraldo06 <eraldoleonardo384@gmail.com>
2022-08-31 22:52:32 -0300

    Foi criado a API ESTADOS

---
+++ API-ESTADOS/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const ListES = require("./database/estados.json");
+
+// função
+const server = http.createServer((req, res)=>{
+
+    // Acesso a todos os servidores
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    
+    res.write(JSON.stringify(ListES));// mudança de JSON para TEXTO
+    return res.end(); // Retorno
+});
+
+// porta do servidor
+server.listen(8000); 
