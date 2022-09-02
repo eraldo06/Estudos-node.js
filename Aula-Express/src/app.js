8ad27c094e5de904cbf96b1eae09da436ff615f5
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-01 22:31:19 -0300

    Organizando Pastas2

--- Aula-Express/src/app.js
+++ Aula-Express/src/app.js
@@ -3,7 +3,7 @@ const express = require("express") // Exportando o Express
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send('Helo'); // resposta para o html
+    res.send('Helo é nada'); // resposta para o html
 });
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000")); // porta servidor
