a58e82151718792c4fb732273a5e327a686071dd
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-03 10:24:13 -0300

    aprendendo rotas

--- Aula-Express/src/app.js
+++ Aula-Express/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express") // Exportando o Express
-const routes = require("./routes")
+const routes = require("./routes") // rota externa
+
 const app = express();
 
-app.use(routes);
+app.use(express.json());
+app.use(routes); // usar as rotas externas
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000")); // porta servidor
--- Aula-Express/src/routes/index.js
+++ Aula-Express/src/routes/index.js
@@ -1,9 +1,23 @@
-const express = require('espress');
+const express = require("express");
 
 const routes = express.Router()
 
-routes.get("/", (req, res)=>{
-    res.send('Helo é nada'); // resposta para o html
+routes.get("/", (req, res) => {
+    res.send(" PEGOU PORRA"); // resposta para o html
+});
+
+
+// pegar informação pela url
+routes.get("/produto/:id/:nome/:idade", (req, res) => {
+    console.log(req.params);
+    res.send("Cadastrado"); 
+});
+
+// pega informação pela url
+routes.post("/cadastrar", (req, res)=>{
+    console.log(req.body);
+    res.send(req.body);
 });
 
 module.exports = routes;
+
